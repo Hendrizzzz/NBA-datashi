baf0e16ace9766bdb90e5401dab5a6e0a652f32d
ShinzouWoSasaygeyo <152076842+ShinzouWoSasaygeyo@users.noreply.github.com>
2025-01-20 21:50:27 +0800

    Update readData method

--- src/service/NbaAnalytics.java
+++ src/service/NbaAnalytics.java
@@ -8,6 +8,32 @@ import model.Team;
 import java.util.List;
 
 public class NbaAnalytics {
+    
+    public static void readData(String filename) {
+        BufferedReader reader = null;
+        String line = "";
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                for (String i : row) {
+                    System.out.printf("%-25s", i);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
     private List<Player> players;
     private List<Team> teams;
 
