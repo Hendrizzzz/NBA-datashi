50659493eb3a0b5f74978a631743e4b901463ed0
JIM HENDRIX BAG-EO <2243493@slu.edu.ph>
2025-01-22 11:02:14 +0800

    Update the service classes

--- src/service/PlayerService.java
+++ src/service/PlayerService.java
@@ -1,11 +1,18 @@
 package service;
 
+import exception.PlayerNotFoundException;
+import exception.TeamNotFoundException;
 import model.Player;
+import model.Team;
+import util.PlayerFilter;
+import util.PlayerSorter;
 
 import java.util.List;
 
 public class PlayerService {
     private List<Player> players;
+
+
 
     public List<Player> getPlayers() {
         return players;
@@ -14,6 +21,59 @@ public class PlayerService {
     public void setPlayers(List<Player> players) {
         this.players = players;
     }
+
+    public List<Player> getPlayersByPosition(String position) {
+        return PlayerFilter.byPosition(players, position);
+    }
+
+    public List<Player> getPlayersByAge(int age) {
+        return PlayerFilter.byAge(players, age);
+    }
+
+    public List<Player> getPlayersByScoreRange(int min, int max) {
+        return PlayerFilter.byScoreRange(players, min, max);
+    }
+
+    public List<Player> getPlayerByTeam(List<Team> teams, String team) throws TeamNotFoundException {
+        return PlayerFilter.byTeam(teams, team);
+    }
+
+    public List<Player> getSortedPlayersByPpg(boolean isAscending) {
+        return PlayerSorter.sortPlayersByPPG(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersByRpg(boolean isAscending) {
+        return PlayerSorter.sortPlayersByRPG(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersByApg(boolean isAscending) {
+        return PlayerSorter.sortPlayersByAPG(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersBySpg(boolean isAscending) {
+        return PlayerSorter.sortPlayersBySPG(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersByBpg(boolean isAscending) {
+        return PlayerSorter.sortPlayersByBPG(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersByTpg(boolean isAscending) {
+        return PlayerSorter.sortPlayersByTPG(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersBySalary(boolean isAscending) {
+        return PlayerSorter.sortPlayersBySalary(players, isAscending);
+    }
+
+    public List<Player> getSortedPlayersByGamesPlayed(boolean isAscending) {
+        return PlayerSorter.sortPlayersByGamesPlayed(players, isAscending);
+    }
+
+    public Player searchPlayer(String name) throws PlayerNotFoundException {
+        return PlayerFilter.byName(players, name);
+    }
+
 
 
 }
--- src/service/TeamService.java
+++ src/service/TeamService.java
@@ -1,4 +1,27 @@
 package service;
 
+import exception.TeamNotFoundException;
+import model.Team;
+import util.TeamFilter;
+
+import java.util.List;
+
 public class TeamService {
+
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+
+
+    public Team searchTeam(String teamName) throws TeamNotFoundException {
+        return TeamFilter.getTeamByName(teams, teamName);
+    }
+
+
 }
